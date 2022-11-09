7e51d8d3c853f70fef5eeb78cb042158e7f455f5
muxiddin <muxiddintemirov@gmail.com>
2022-11-09 11:27:36 +0500

    01 bajarildi

--- file01.py
+++ file01.py
@@ -1,3 +1,6 @@
+f = open('txt_file/data01.txt')
+a=f.read()
+b=a.split(',')
 def main(data:str):
     """
     The data is from the file. Return data as a list type.
@@ -6,5 +9,6 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    return b
+print(main(a))
 # Read data from file
